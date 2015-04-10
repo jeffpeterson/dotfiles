fd6012b9f53b1c7530a411edd5720ca3236daf78
Jeff Peterson <jeff@petersonj.com>
2015-04-10 14:58:54 -0700

    brew bundle is goooone

---
+++ Brewfile.sh
@@ -0,0 +1,44 @@
+brew install caskroom/cask/brew-cask
+
+brew cask install 1password
+brew cask install iterm2
+brew cask install alfred
+brew cask install atom
+brew cask install google-chrome
+brew cask install slate
+# brew cask install dropbox
+# brew cask install airfoil
+# brew cask install xscope
+
+brew tap homebrew/dupes
+
+brew install jq
+brew install wget
+brew install zsh
+brew install bash
+brew install tmux
+# brew install reattach-to-user-namespace
+brew install git
+brew install make
+brew install rbenv
+brew install ruby-build
+brew install the_silver_searcher
+brew install node
+# brew install heroku-toolbelt
+# brew install imagemagick
+# brew install autotrace
+# brew install minidlna
+# brew install phantomjs
+# brew install postgresql
+# brew install ttyrec
+brew install vim
+brew install watchman
+brew install bin-utils
+# brew install htop-osx
+# brew install par
+
+# brew unused
+# brew install geoip gmp go libsndfile libusb openvpn pyqt python sdl_image sdl_mixer sdl_ttf portmidi yajl
+
+# brew work
+# brew install memcached elasticsearch mongodb mysql rabbitmq redis
