84f53c41260673dc597739b29ffdda7dfb966a6e
Jeff Peterson <jeff@petersonj.com>
2015-04-10 23:47:31 -0700

    remove subl, Makefile. Add install.sh, iterm2.plist

--- .subl/plugins.py
+++
@@ -1,5 +0,0 @@
-import sublime, sublime_plugin
-
-class ScopeToClipboardCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		sublime.set_clipboard(self.view.syntax_name(self.view.sel()[0].b))
---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+for f in `ls -A | grep -ve Brewfile.sh -e Preferences -e install.sh -e .git`
+do
+  ln -hs "$PWD/$f" "$HOME/$f"
+done
+
+# TODO: Figure out how to make something like this work:
+# for f in `ls Preferences`
+# do
+#   ln "$PWD/Preferences/$f" "$HOME/Library/Preferences/$f"
+# done
