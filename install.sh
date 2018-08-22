a677a1a55ef7d2dc7e674ebacc774dd30a734ee4
Jeff Peterson <jeff@yak.sh>
2018-08-22 12:37:09 -0700

    Update install script

--- install.sh
+++ install.sh
@@ -1,9 +1,29 @@
 #!/usr/bin/env bash
 
-for f in `ls -A home`
-do
-  ln -ns "$PWD/home/$f" "$HOME/$f"
-done
+local install_all() {
+  install_home home
+  # install_home `osname`
+}
+
+local install_home() {
+  for f in `ls -A $1`
+  do
+    ln -ns "$PWD/$1/$f" "$HOME/$f"
+  done
+}
+
+
+local osname() {
+  uname -a | cut -d " " -f 1 | tr '[:upper:]' '[:lower:]'
+}
+
+install_all
+
+
+# list() {
+#    find `pwd -P`/$1 -maxdepth 1 -mindepth 1
+# }
+
 
 # TODO: Figure out how to make something like this work:
 # for f in `ls Preferences`
