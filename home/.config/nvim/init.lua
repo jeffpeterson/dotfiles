01de86c35e00f736663714944d1bcbdb07e2ae85
Jeff Peterson <jeff@yak.sh>
2026-06-03 13:42:02 -0400

    many nvim options

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,7 +1,15 @@
+-- vim:foldmethod=marker
+
+-- OPTIONS {{{
+-- :help option-list
+--
 -- See `:h mapleader`
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.o.ignorecase = true
 vim.o.smartcase  = true
+
+vim.o.tabstop    = 2
+vim.o.shiftwidth = 2
 
 vim.o.wrap           = false
 vim.o.cursorline     = true -- Highlight the line where the cursor is on.
@@ -13,8 +21,9 @@ vim.o.relativenumber = true -- Show relative line numbers except current
 -- If performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s). See `:h 'confirm'`
 vim.o.confirm = true
+-- }}}
 
--- KEYMAPS
+-- KEYMAPS {{{
 --
 -- See `:h vim.keymap.set()`, `:h mapping`, `:h keycodes`
 
@@ -25,18 +34,10 @@ vim.keymap.set({'i'}, 'jk', '<esc>')
 vim.keymap.set({'i'}, 'JK', '<esc>')
 vim.keymap.set({'i'}, 'jK', '<esc>')
 vim.keymap.set({'i'}, 'Jk', '<esc>')
+--
+-- }}}
 
--- Map <A-j>, <A-k>, <A-h>, <A-l> to navigate between windows in any modes
-vim.keymap.set({ 't', 'i' }, '<A-h>', '<C-\\><C-n><C-w>h')
-vim.keymap.set({ 't', 'i' }, '<A-j>', '<C-\\><C-n><C-w>j')
-vim.keymap.set({ 't', 'i' }, '<A-k>', '<C-\\><C-n><C-w>k')
-vim.keymap.set({ 't', 'i' }, '<A-l>', '<C-\\><C-n><C-w>l')
-vim.keymap.set({ 'n' }, '<A-h>', '<C-w>h')
-vim.keymap.set({ 'n' }, '<A-j>', '<C-w>j')
-vim.keymap.set({ 'n' }, '<A-k>', '<C-w>k')
-vim.keymap.set({ 'n' }, '<A-l>', '<C-w>l')
-
--- AUTOCOMMANDS (EVENT HANDLERS)
+-- AUTOCOMMANDS (EVENT HANDLERS) {{{
 --
 -- See `:h lua-guide-autocommands`, `:h autocmd`, `:h nvim_create_autocmd()`
 
@@ -49,7 +50,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- USER COMMANDS: DEFINE CUSTOM COMMANDS
+-- }}}
+
+-- USER COMMANDS: DEFINE CUSTOM COMMANDS {{{
 --
 -- See `:h nvim_create_user_command()` and `:h user-commands`
 
@@ -60,7 +63,9 @@ vim.api.nvim_create_user_command('GitBlameLine', function()
   print(vim.system({ 'git', 'blame', '-L', line_number .. ',+1', filename }):wait().stdout)
 end, { desc = 'Print the git blame for the current line' })
 
--- PLUGINS
+-- }}}
+
+-- PLUGINS {{{
 --
 -- See `:h :packadd`, `:h vim.pack`
 
@@ -70,20 +75,41 @@ vim.cmd('packadd! nohlsearch')
 
 -- Install third-party plugins via "vim.pack.add()".
 vim.pack.add({
+  'https://github.com/MunifTanjim/nui.nvim',          -- UI component library
+  'https://github.com/nvim-lua/plenary.nvim',         -- String helpers
+  'https://github.com/nvim-tree/nvim-web-devicons',   -- Nerd Font icon support
+
+  'https://github.com/tpope/vim-sleuth',
+
+  -- 'https://github.com/ibhagwan/fzf-lua',           -- Fuzzy picker
+  'https://github.com/nvim-neo-tree/neo-tree.nvim',   -- File explorer
+  'https://github.com/nvim-telescope/telescope.nvim', -- Fuzzy picker
   'https://github.com/neovim/nvim-lspconfig',     -- Quickstart configs for LSP
-  'https://github.com/ibhagwan/fzf-lua',          -- Fuzzy picker
   'https://github.com/nvim-mini/mini.completion', -- Autocompletion
-  'https://github.com/nvim-mini/mini.align',      -- Alignment
+  'https://github.com/nvim-mini/mini.align',      -- Alignment. Ex: vip ga=
   'https://github.com/stevearc/quicker.nvim',     -- Enhanced quickfix/loclist
   'https://github.com/lewis6991/gitsigns.nvim',   -- Git integration
   'https://github.com/neanias/everforest-nvim',   -- Color scheme
-  'https://github.com/kylechui/nvim-surround',    -- Ex: cs"'
+  'https://github.com/kylechui/nvim-surround',    -- Quote wrapping. Ex: cs"'
 })
 
-require('fzf-lua').setup { fzf_colors = true }
+-- require('fzf-lua').setup { fzf_colors = true }
+require('neo-tree').setup {
+  filesystem = {
+    filtered_items = {
+      visible = true,
+      hide_dotfiles = false,
+    }
+  }
+}
+require('telescope').setup {}
 require('mini.completion').setup {}
 require('mini.align').setup {}
 require('quicker').setup {}
-require('gitsigns').setup {}
+require('gitsigns').setup {
+  current_line_blame = false
+}
 require('everforest').load()
 require('nvim-surround').setup {}
+
+-- }}}
